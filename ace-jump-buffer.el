f169b4de4ad21ab3f9adf6d5b647b86b246271eb
justin talbott <justin@waymondo.com>
2014-09-22 22:36:43 -0400

    convert internal variable naming convention, remove unneeded C-g binding

--- ace-jump-buffer.el
+++ ace-jump-buffer.el
@@ -66,34 +66,34 @@
   :group 'ace-jump-buffer)
 
 ;; interval settings
-(defvar ajb--showing nil)
-(defvar ajb--other-window nil)
-(defvar ajb--in-one-window nil)
+(defvar ajb/showing nil)
+(defvar ajb/other-window nil)
+(defvar ajb/in-one-window nil)
 
 ;; settings for a barebones `bs' switcher
-(defvar ajb--bs-attributes-list '(("" 2 2 left " ")
-                                  ("" 1 1 left bs--get-marked-string)
-                                  ("" 1 1 left " ")
-                                  ("Buffer" bs--get-name-length 10 left bs--get-name)))
+(defvar ajb/bs-attributes-list '(("" 2 2 left " ")
+                                 ("" 1 1 left bs--get-marked-string)
+                                 ("" 1 1 left " ")
+                                 ("Buffer" bs--get-name-length 10 left bs--get-name)))
 
 
 (defadvice bs--show-header (around maybe-disable-bs-header activate)
   "Don't show the `bs' header when doing `ace-jump-buffer'"
-  (unless ajb--showing ad-do-it))
+  (unless ajb/showing ad-do-it))
 
 (defun ace-jump-buffer-hook ()
   "On the end of ace jump, select the buffer at the current line."
   (when (string-match (buffer-name) "*buffer-selection*")
-    (if ajb--other-window (bs-select-other-window)
-      (if ajb--in-one-window (bs-select-in-one-window)
+    (if ajb/other-window (bs-select-other-window)
+      (if ajb/in-one-window (bs-select-in-one-window)
         (bs-select)))
     (ace-jump-buffer-reset)))
 
 (add-hook 'ace-jump-mode-end-hook 'ace-jump-buffer-hook)
 
 (defun ace-jump-buffer-reset ()
-  (setq ajb--other-window nil)
-  (setq ajb--in-one-window nil)
+  (setq ajb/other-window nil)
+  (setq ajb/in-one-window nil)
   (kill-buffer "*buffer-selection*"))
 
 ;;;###autoload
@@ -103,8 +103,8 @@
   (let ((ace-jump-mode-gray-background nil)
         (ace-jump-mode-scope 'window)
         (bs-buffer-sort-function ajb-sort-function)
-        (bs-attributes-list ajb--bs-attributes-list)
-        (ajb--showing t))
+        (bs-attributes-list ajb/bs-attributes-list)
+        (ajb/showing t))
     (bs--show-with-configuration ajb-bs-configuration)
     (set (make-local-variable 'bs-header-lines-length) 0)
     (set (make-local-variable 'bs-max-window-height) ajb-max-window-height)
@@ -112,28 +112,27 @@
     (goto-char (point-min))
     (bs--set-window-height)
     (call-interactively 'ace-jump-line-mode)
-    (define-key overriding-local-map (kbd "C-g") 'ace-jump-buffer-exit)
     (define-key overriding-local-map [t] 'ace-jump-buffer-exit)))
 
 ;;;###autoload
 (defun ace-jump-buffer-other-window ()
   "Quickly hop to buffer with `ace-jump-mode' in other window."
   (interactive)
-  (setq ajb--other-window t)
+  (setq ajb/other-window t)
   (ace-jump-buffer))
 
 ;;;###autoload
 (defun ace-jump-buffer-in-one-window ()
   "Quickly hop to buffer with `ace-jump-mode' in one window."
   (interactive)
-  (setq ajb--in-one-window t)
+  (setq ajb/in-one-window t)
   (ace-jump-buffer))
+
 (defun bs-sort-buffers-by-recentf (b1 b2)
   "Function for sorting buffers by recentf order."
   (let ((b1-index (-elem-index (buffer-file-name b1) recentf-list))
         (b2-index (-elem-index (buffer-file-name b2) recentf-list)))
     (when (< b1-index b2-index) t)))
-
 
 (defun ace-jump-buffer-exit ()
   (interactive)
